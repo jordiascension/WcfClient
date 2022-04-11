b8a76c4ded3c8aa3754ba83753f598157142ce8c
jordiascension <jordiascension@enterpriseconsulting.es>
2022-04-11 09:06:59 +0200

    First Commit

---
+++ VuelingClientWcf/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using VuelingClientWcf.WebReference;
+
+namespace VuelingClientWcf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WebReference.IStudentWebService svc = new WebReference.StudentWebServiceClient();
+            try
+            {
+                
+                MessageBox.Show(svc.GetData(0, null).ToString());
+                
+            }
+            catch (FaultException<ValidationFault> ex)
+            {
+                foreach (var details in ex.Detail.Details)
+                {
+                    MessageBox.Show(details.Message);
+                }
+            }
+
+            try
+            {
+                MessageBox.Show(svc.ConcatStrings(null, null));
+            }
+            catch (FaultException<ValidationFault> ex)
+            {
+                foreach (var details in ex.Detail.Details)
+                {
+                    MessageBox.Show(details.Message);
+                }
+            }
+
+
+        }
+    }
+}
